f0750c95195b7de92144c8b2cd53fe793b877756
sipon <fakhrulislamsipon@gmail.com>
2025-06-14 16:45:39 +0600

    implement add course post method

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-// name: Course-Management-Db
-// password: EUh4MB7wpGXtR92i
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zffyl01.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,12 +23,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const courseCollection = client.db("courseDB").collection("courses");
+
+    
+  app.post('/add-course', async(req, res) => {
+    const courseData = req.body
+    const result = await courseCollection.insertOne(courseData)
+    res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
